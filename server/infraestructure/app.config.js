6566b8bab2b46a3a5ccff44c4c2ab65c32c44da9
Julio Alberto Cano Lopez <jcanol@rentingcolombia.com>
2021-05-13 22:16:11 -0500

    configuring string connections and updating versions

--- server/infraestructure/app.config.js
+++ server/infraestructure/app.config.js
@@ -1,12 +1,12 @@
 // PORT
 process.env.PORT = process.env.PORT || 3000;
 // ENVIRONMENT
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+process.env.NODE_ENV = process.env.NODE_ENV || 'test'
 // SEED
 process.env.SEED = 'este-es-el-key'
 // DATABASE
 let urlDB;
 if (process.env.NODE_ENV === 'dev')
   urlDB = 'mongodb://localhost:27017/Cafe';
-else urlDB = 'mongodb://jrgongdev:0Urv2JtPtVgHwID7@cluster0-shard-00-00-yryv4.mongodb.net:27017,cluster0-shard-00-01-yryv4.mongodb.net:27017,cluster0-shard-00-02-yryv4.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+else urlDB = 'mongodb+srv://jrgongAdmin:ViQMgLcx5OUqEYtI@cluster0.mgfgz.mongodb.net/universityDatabase?retryWrites=true&w=majority';
 process.env.URLBD = urlDB;
